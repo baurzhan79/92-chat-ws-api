57a0ec728cd2b16efe285407fb3fe7f6e339b05e
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-08-13 16:01:34 +0600

    A fixture with initial user messages has been created

--- fixtures.js
+++ fixtures.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const { nanoid } = require("nanoid");
 
-const User = require("./app/models/User")
+const User = require("./app/models/User");
+const ChatMessages = require("./app/models/ChatMessages");
 
 mongoose.connect("mongodb://localhost/chat-ws");
 const db = mongoose.connection;
@@ -9,11 +10,13 @@ const db = mongoose.connection;
 db.once("open", async () => {
     try {
         await db.dropCollection("users");
+        await db.dropCollection("chatmessages");
+
     } catch (e) {
         console.log("Collections were not present, skipping drop...");
     }
 
-    await User.create(
+    const [UserUser, UserAdmin] = await User.create(
         {
             username: "user",
             email: "user@chatWs.com",
@@ -27,6 +30,140 @@ db.once("open", async () => {
             password: "123456",
             token: nanoid(),
             role: "admin"
+        }
+    );
+
+    await ChatMessages.create(
+        {
+            text: "message 1",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 2",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 3",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 4",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 5",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 6",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 7",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 8",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 9",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 10",
+            author: UserAdmin._id,
+            datetime: new Date()
+        }
+    );
+
+    await ChatMessages.create(
+        {
+            text: "message 11",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 12",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 13",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 14",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 15",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 16",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 17",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 18",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 19",
+            author: UserUser._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 20",
+            author: UserUser._id,
+            datetime: new Date()
+        }
+    );
+
+    await ChatMessages.create(
+        {
+            text: "message 21",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 22",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 23",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 24",
+            author: UserAdmin._id,
+            datetime: new Date()
+        },
+        {
+            text: "message 25",
+            author: UserAdmin._id,
+            datetime: new Date()
         }
     );
 
